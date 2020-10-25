396c25276adbfaac6e761d5be8be586c17dec96a
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-25 21:11:32 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var cells = document.getElementsByTagName("td");
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+setInterval(grav, 100);
+for (var i = 0; i < 625; i++) {
+    cells[i].id = String(alphabet[Math.floor(i / 25)]) + String((i % 25) + 1);
+}
+function place() {
+  document.getElementById(document.getElementById("input").value).classList.add("on");
+}
+function grav() {
+  for (var o = 624; o >= 0; --o) {
+    if (cells[o].classList.contains("on")) {
+      if (alphabet.indexOf(cells[o].id.charAt(0)) < 24) {
+        if (!document.getElementById(String((alphabet[alphabet.indexOf(cells[o].id.charAt(0)) + 1] + cells[o].id.substring(1)))).classList.contains("on")) {
+          document.getElementById((alphabet[alphabet.indexOf(cells[o].id.charAt(0)) + 1] + cells[o].id.substring(1))).classList.add("on");
+          cells[o].classList.remove("on");
+        }
+      }
+    }
+  }
+}
+function reset() {
+  for (var i = 0; i < 625; i++) {
+    cells[i].classList.remove("on");
+  }
+}
