33fa13659f67fb93dfe8f7819c4233f008fd87a6
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-26 19:33:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var cells = Array(document.getElementsByTagName("td"));
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 for (var i = 0; i < 625; i++) {
-    console.log(cells[0]);
+    console.log(cells[i]);
     cells[i].id = String(alphabet[Math.floor(i / 25)]) + String((i % 25) + 1);
 }
 setInterval(grav, 100);
@@ -25,4 +25,3 @@ function reset() {
     cells[i].classList.remove("on");
   }
 }
-console.log(cells[0]);
