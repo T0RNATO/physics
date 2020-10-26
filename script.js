4a96e8e3b3b6f1c00db66a8080aa11e8d58a7802
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-26 19:11:03 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var cells = document.getElementsByTagName("td");
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 setInterval(grav, 100);
-for (var i = 0; i < 625; i++) {
+for (var i = 0; i < 624; i++) {
     cells[i].id = String(alphabet[Math.floor(i / 25)]) + String((i % 25) + 1);
 }
 function place() {
