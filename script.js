c3f049ecf5982e64cea7592ffdd4bbb75bd62019
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-27 11:34:17 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var cells = Array(document.getElementsByTagName("td"));
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-for (var i = 0; i < 625; i++) {
+window.onload = function() {
+  var cells = Array(document.getElementsByTagName("td"));
+  for (var i = 0; i < 625; i++) {
     console.log(cells);
     console.log(i);
     cells[i].id = String(alphabet[Math.floor(i / 25)]) + String((i % 25) + 1);
-}
-setInterval(grav, 100);
+  }
+  setInterval(grav, 100);
+};
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 function place() {
   document.getElementById(document.getElementById("input").value).classList.add("on");
 }
