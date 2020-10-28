a5b38f7b6a4eb70e45e4a343d72ea9be07648020
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-28 11:36:13 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var trcont = "<tr>" + String("<td onclick='summon(this)'></td>").repeat(25) + "</tr>";
+document.getElementById("thingo").innerHTML = trcont.repeat(25);
 var cells = document.getElementsByTagName("td");
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"];
 setInterval(grav, 100);
