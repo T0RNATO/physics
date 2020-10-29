2902582c71e4e31dcd7f5095bb155e82648259cf
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-30 10:20:46 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var trcont = "<tr>" + String("<td onclick='summon(this)'></td>").repeat(25) + "</tr>";
+var trcont = "<tr>" + String("<td onclick='summon(this)'></td>").repeat(50) + "</tr>";
 document.getElementById("thingo").innerHTML = trcont.repeat(25);
 var cells = document.getElementsByTagName("td");
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"];
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 setInterval(grav, 100);
-for (var i = 0; i < 625; i++) {
+for (var i = 0; i < 1250; i++) {
     cells[i].id = String(alphabet[Math.floor(i / 25)]) + String((i % 25) + 1);
 }
 function grav() {
-  for (var o = 624; o >= 0; --o) {
+  for (var o = 1249; o >= 0; --o) {
     if (cells[o].classList.contains("on")) {
       if (alphabet.indexOf(cells[o].id.charAt(0)) < 24) {
         if (!document.getElementById(String((alphabet[alphabet.indexOf(cells[o].id.charAt(0)) + 1] + cells[o].id.substring(1)))).classList.contains("on")) {
@@ -19,7 +19,7 @@ function grav() {
   }
 }
 function reset() {
-  for (var i = 0; i < 625; i++) {
+  for (var i = 0; i < 1250; i++) {
     cells[i].classList.remove("on");
   }
 }
